09578101a4766cdd27409010ed674320c1e775eb
DarkZelotesTV <113208147+DarkZelotesTV@users.noreply.github.com>
2025-12-18 21:42:18 +0100

    Remove chat components and references

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -1,13 +1,11 @@
 import { HashRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { MainLayout } from './components/layout/MainLayout';
-import { StandaloneChat } from './components/popouts/StandaloneChat';
 
 function App() {
   return (
     <HashRouter>
       <Routes>
         <Route path="/" element={<MainLayout />} />
-        <Route path="/popout/:chatId" element={<StandaloneChat />} />
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </HashRouter>
