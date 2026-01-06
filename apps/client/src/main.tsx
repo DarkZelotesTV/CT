43cc0b4c6443197b68bba924ad038db9ad1b6bbf
DarkZelotesTV <113208147+DarkZelotesTV@users.noreply.github.com>
2026-01-06 01:25:42 +0100

    Wrap client app with top bar provider

--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -9,17 +9,20 @@ import { SettingsProvider } from './context/SettingsContext';
 import { VoiceProvider } from './features/voice';
 import { I18nextProvider } from 'react-i18next';
 import i18n from './i18n/config';
+import { TopBarProvider } from './components/window/TopBarContext';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <IdentityGate>
-    <SettingsProvider>
-      <SocketProvider>
-        <VoiceProvider>
-          <I18nextProvider i18n={i18n}>
-            <App />
-          </I18nextProvider>
-        </VoiceProvider>
-      </SocketProvider>
-    </SettingsProvider>
-  </IdentityGate>,
+  <TopBarProvider>
+    <IdentityGate>
+      <SettingsProvider>
+        <SocketProvider>
+          <VoiceProvider>
+            <I18nextProvider i18n={i18n}>
+              <App />
+            </I18nextProvider>
+          </VoiceProvider>
+        </SocketProvider>
+      </SettingsProvider>
+    </IdentityGate>
+  </TopBarProvider>,
 );
