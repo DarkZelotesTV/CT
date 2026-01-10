90814928f52c712d9369159cf98bc828939174ef
DarkZelotesTV <113208147+DarkZelotesTV@users.noreply.github.com>
2026-01-10 02:15:35 +0100

    Allow local socket origin and iframe CSP

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -88,6 +88,10 @@ const defaultCorsOrigins = [
   'https://127.0.0.1:5173',
   'http://localhost:5173',
   'http://127.0.0.1:5173',
+  'https://localhost:3001',
+  'https://127.0.0.1:3001',
+  'http://localhost:3001',
+  'http://127.0.0.1:3001',
 ];
 
 const allowedHttpOrigins = uniqueConfiguredCorsOrigins.length
